b7d421e417bd898087ded8cb04076e146ca63360
root <root@LAPTOP-7PI6R8OK>
2023-12-02 16:48:37 +0200

    version 8

--- crawler.py
+++ crawler.py
@@ -1,133 +1,72 @@
-'''
-This is Raviv's and Noam's project :)
-'''
+#This is Raviv and Noam's project :)
 
-import argparse
 import urllib.request
+import urllib.parse
+import html.parser
+import argparse
 import yaml
+import sys
 import re
 
-#gets input from user
-parser = argparse.ArgumentParser()
-parser.add_argument('-d', '--depth', type=int, default=2)
-parser.add_argument('-ir', '--ignore_regex', type=str, default="^$") #compatible only with the string: "", there is no url like this!
-parser.add_argument('url', type=str)
-arguments = parser.parse_args()
 
-depth = arguments.depth
-regex = arguments.ignore_regex
-url = arguments.url
+class MyHTMLParser(html.parser.HTMLParser):
+    def __init__(self):
+        super().__init__()
+        self.current_links = []
+        self.all_links = []
+        self.url = ""
 
-
-def scan_urls_href(html_code, src, regex):
-    '''
-    return a list of all the urls from a url (html_code)
-    using attribute src
-    '''
-    last_index = 0
-    urls_lst = []
-    while last_index < len(html_code):
-        current_index = html_code.find('href="', last_index) #index of h
-        if current_index != -1:
-            j = current_index + 6
-            lst = []
-            while html_code[j] != '"':
-                lst.append(html_code[j])
-                j += 1
-            last_index = j + 1
-            #makes the links readable by the library
-            if lst[0] == '/' and lst[1] != '/':
-                link = src + "".join(lst)
-            elif lst[0] == '/' and lst[1] == '/':
-                if 's' == src[4]:
-                    link = 'https:' + "".join(lst)
-                else:
-                    link = 'http:' + ''.join(lst)
-            else:
-                link = "".join(lst)
-            if link not in urls_lst and (not re.search(regex, link)):
-                urls_lst.append(link)
-        else:
-            last_index = len(html_code)
-    return urls_lst
-
-
-def scan_urls_src(html_code, src, new_lst, regex):
-    '''
-    return a list of all the urls from a url (html_code)
-    using attribute src
-    '''
-    last_index = 0
-    url_lst = []
-    while last_index < len(html_code):
-        current_index = html_code.find('src="', last_index)
-        if current_index != -1:
-            j = current_index = 6
-            lst = []
-            while html_code[j] != '"':
-                lst.append(html_code[j])
-                j += 1
-            last_index = j + 1
-            #makes the links readable by the library
-            if lst[0] == '/' and lst[1] != '/':
-                link = src + "".join(lst)
-            elif lst[0] == '/' and lst[1] == '/':
-                if 's' == src[4]:
-                    link = 'https:' + "".join(lst)
-                else:
-                    link = 'http:' + "".join(lst)
-            else:
-                link = "".join(lst)
-            if link not in urls_lst and link not in new_lst and (not re.search(regex, link)):
-                urls_lst.append(link)
-        else:
-            last_index = len(html)
-    return urls_lst
-
-
-def get_all_urls(regex, depth, url):
-    '''
-    the main function
-    defines the dictionary
-    adds the urls
-    converts to yaml
-    '''
-    temp = [{"level":i, "links":[]} for i in range(depth+1)]
-    dictionary = {"root":url, "web":temp}
-    dictionary["web"][0]["links"] = [url]
-
-    for i in range(1, depth+1):
-        lst = []
-        for j in dictionary["web"][i-1]["links"]:
-            #try so we dont get a 404/403 error
-            try:
-                data = str(urllib.request.urlopen(j).read())
-                src_site = get_site_name(j)
-                lst += scan_urls_href(data, src_site, regex)
-                lst += scan_urls_src(data, src_site, lst, regex)
-            except:
+    def handle_starttag(self, tag, attrs):
+        for name, value in attrs:
+            if not name in ('href', 'src'):
                 continue
-        dictionary["web"][i]["links"] = lst
 
-    return yaml.dump(dictionary)
+            link = urllib.parse.urljoin(self.url, value)
+
+            if re.search(arguments.ignore_regex, link):
+                continue
+
+            if not link in self.all_links:
+                self.current_links.append(link)
+                self.all_links.append(link)
 
 
-def get_site_name(url):
-    '''
-    get the name of the parent site.
-    example: https://en.wikipedia.org/wiki/Chess => https://en.wikipedia.org
-    '''
-    counter = 0
-    index = 0
-    while index < len(url):
-        if url[index] == '/':
-            counter += 1
-        if counter == 3:
-            return url[0:index]
-        index += 1
-    return url
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--depth', type=int, default=2)
+    parser.add_argument('-ir', '--ignore_regex', type=str, default="^$")
+    parser.add_argument('url', type=str)
+    arguments = parser.parse_args()
 
-#calls the main funtion and prints the yaml
-#doesn't work for non http/https site because of library limitations
-print(get_all_urls(regex, depth, url))
+    HTMLparser = MyHTMLParser()
 
+
+def scan_urls(url):
+    try:
+        HTMLparser.url = url
+        HTMLparser.feed(str(urllib.request.urlopen(url).read()))
+        links = HTMLparser.current_links.copy()
+        HTMLparser.current_links = []
+        return links
+    except:
+        return []
+
+
+def get_web_of_links():
+    result = {"root":arguments.url, "web":[{"level":i, "links":[]} for i in range(arguments.depth+1)]}
+    result["web"][0]["links"] = [arguments.url]
+
+    for i in range(arguments.depth):
+        for link in result["web"][i]["links"]:
+            result["web"][i+1]["links"] += scan_urls(link)
+
+        if len(result["web"][i+1]["links"]) == 0:
+            for j in range(arguments.depth, i, -1):
+                result["web"].pop(j)
+            break
+
+    return result
+
+
+if __name__ == "__main__":
+    yaml.dump(get_web_of_links(), stream=sys.stdout)
